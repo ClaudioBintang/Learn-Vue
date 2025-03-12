415dbbdb899809d1773116488c25e5967c08904d
Claudio Bintang Wibowo <claudiowibowo90@gmail.com>
2025-03-12 22:06:29 +0700

    learn ref and reactive in vue

---
+++ vue-day2/vue-project2/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+const name = "Claudio" //data statis, tidak dapat berubah
+const age = ref(true) //data dinamis, dapat berubah, sama seperti state di react
+const sidebar = ref(true)
+const handleClick = () => {
+  age.value = !age.value
+  console.log(age.value)
+}
+const handleSidebar = () => {
+  sidebar.value = !sidebar.value
+}
+</script>
+
+<template>
+  <Sidebar />
+  <div>
+    <h3>App.vue, hello there!</h3>
+    <p>my name is {{ name }}</p>
+  </div>
+  <div>
+    <button @click="handleClick">Click</button>
+    <p v-if="age">18</p>
+    <p v-else>woi tuwa</p>
+  </div>
+  <button v-if="!sidebar" @click="handleSidebar">=</button>
+  <Sidebar v-if="sidebar" :handleSidebar="handleSidebar"/> 
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-day2/vue-project2/src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+    handleSidebar: {
+        type: Function,
+    }
+})
+</script>
+
+<template>
+<button @click="handleSidebar">x</button>
+  <div>
+    <p> sidebar </p>
+    <p> menu </p>
+    <p> Contact </p>
+  </div>
+
+</template>
+<style scoped>
+
+</style>
